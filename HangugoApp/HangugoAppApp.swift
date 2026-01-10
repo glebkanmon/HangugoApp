45518e783a37deff0a2ea69a448be8829ab3e36d
g.monetchikov <g.monetchikov@tinkoff.ru>
2026-01-10 14:30:16 +0300

    Initial Commit

---
+++ HangugoApp/HangugoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangugoAppApp.swift
+//  HangugoApp
+//
+//  Created by Gleb Monetchikov on 10.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct HangugoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
